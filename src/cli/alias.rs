1f213d04f3ab4514c6e5c37a6a40830a890fa961
Joeky <joeky5888@gmail.com>
2021-04-07 00:00:58 +0800

    Format alias string
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- src/cli/alias.rs
+++ src/cli/alias.rs
@@ -4,13 +4,16 @@ pub struct Alias {}
 
 impl Alias {
     pub fn new() -> App<'static, 'static> {
-        App::new("alias")
-            .about("alias gen")
+        App::new("alias").about("alias gen")
     }
 
     pub fn gen() {
-        println!("alias docker='ufc docker'
-alias ping='ufc ping'"
-        );
+        let subcommands = vec!["docker", "ping"];
+        let mut alias = String::new();
+        for cmd_str in subcommands.iter() {
+            alias.push_str(format!("alias {}='ufc {}'\n", cmd_str, cmd_str).as_str());
+        }
+        alias.pop();
+        println!("{}", alias);
     }
 }
