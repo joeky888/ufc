3de37d691579e47ebdc79ffcfc202de9b013c365
Joeky <joeky5888@gmail.com>
2021-04-07 22:57:04 +0800

    Add docker command args
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- src/cli/docker.rs
+++ src/cli/docker.rs
@@ -1,16 +1,25 @@
-use clap::{App, AppSettings, ArgMatches};
+use clap::{App, Arg, ArgMatches};
 mod images;
 mod ps;
 use images::Images;
 use ps::Ps;
+
+use super::cli::exec;
 
 pub struct Docker {}
 
 impl Docker {
     pub fn new() -> App<'static, 'static> {
         App::new("docker")
-            .setting(AppSettings::NeedsSubcommandHelp)
-            .setting(AppSettings::SubcommandRequiredElseHelp)
+            .args(&[
+                Arg::with_name("config").long("config").takes_value(true).help("Print version information and quit"),
+                Arg::with_name("version").long("version").short("v").help("Print version information and quit"),
+                Arg::with_name("debug").long("debug").short("D").help("Enable debug mode"),
+                Arg::with_name("host").long("host").short("H").takes_value(true).help("Daemon socket(s) to connect to"),
+                Arg::with_name("log").long("log-level").short("l").takes_value(true).help(r#"Set the logging level ("debug"|"info"|"warn"|"error"|"fatal") (default "info")"#),
+            ])
+            // .setting(AppSettings::NeedsSubcommandHelp)
+            // .setting(AppSettings::SubcommandRequiredElseHelp)
             .subcommands(vec![Ps::new(), Images::new()])
             .about("docker")
     }
@@ -19,7 +28,7 @@ impl Docker {
         match app.subcommand() {
             ("ps", Some(args)) => Ps::parse(args),
             ("images", Some(args)) => Images::parse(args),
-            _ => println!("Unsupported command"),
+            _ => exec(vec![]),
         }
     }
 }
