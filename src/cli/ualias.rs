6f31cf70784ee9e32b4ef7eb69e427206192e666
Joeky <joeky5888@gmail.com>
2021-04-16 21:01:01 +0800

    Alias dig command
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- src/cli/ualias.rs
+++ src/cli/ualias.rs
@@ -4,11 +4,12 @@ pub struct Cmd {}
 
 impl Cmd {
     pub fn new() -> App<'static, 'static> {
-        App::new("ualias").about("ualias gen (like alias but with a 'u' prefixed for each subcommand)")
+        App::new("ualias")
+            .about("ualias gen (like alias but with a 'u' prefixed for each subcommand)")
     }
 
     pub fn gen() {
-        let subcommands = vec!["df", "docker", "du", "fdisk", "ping"];
+        let subcommands = vec!["df", "docker", "dig", "du", "fdisk", "ping"];
         let mut alias = String::new();
         for cmd_str in subcommands.iter() {
             alias.push_str(format!("alias u{}='ufc {}'\n", cmd_str, cmd_str).as_str());
