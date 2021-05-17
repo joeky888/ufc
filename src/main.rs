6711baee38cebfe2c5830cad873a7bad05e46184
Joeky <joeky5888@gmail.com>
2021-05-17 10:42:52 +0800

    Update lint
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 #![forbid(unsafe_code)]
+#![deny(unused_imports)]
+#![deny(unused_must_use)]
+#![deny(dead_code)]
+#![deny(clippy::all, clippy::perf, clippy::nursery, clippy::pedantic)]
+#![deny(clippy::filetype_is_file)]
+#![deny(clippy::cargo)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::panic)]
+#![deny(clippy::match_like_matches_macro)]
+#![deny(clippy::needless_update)]
 
 use std::io;
 
